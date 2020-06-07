931f1dd5418b1c2107b46694a74b38ffa23e35f5
Prashant Kumar <prashantkumar@Prashants-MacBook-Pro-2.local>
2020-06-07 14:40:00 +0530

    Add humidity

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -9,7 +9,7 @@ const forcast = (latitude,longitude, callback) => {
         } else if(body.error){
             callback('Not able to find location',undefined)
         }else{
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Humidity is ' + body.current.humidity + '%.')
         }
     })
 }
